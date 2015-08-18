12b7dcecda8823536341a3f1f40148687f809639
priyankpuri <priyankpuri@hotmail.com>
2015-08-19 05:02:06 +0530

    Added new code for inverse of matrix and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This function will help us set up functions like set,get,setrev and getrev which will help us in caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setrev <- function(rev) m <<- rev
+  getrev <- function() m
+  list(set = set, get = get,
+       setrev = setrev,
+       getrev = getrev)
 }
 
 
-## Write a short comment describing this function
+## This function will help us calculate the reverse of the matrix with the above defined functions
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' and displa message if cached data is retrieved
+  m <- x$getrev()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ##to get the matrix
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setrev(m)
+  m
 }
